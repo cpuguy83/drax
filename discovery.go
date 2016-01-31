69b95182eef7afc7b34a44b8091f5f63c6c0454c
Brian Goff <cpuguy83@gmail.com>
2016-01-30 23:06:04 -0500

    remove unused peers struct

--- discovery.go
+++ discovery.go
@@ -1,11 +1,6 @@
 package drax
 
-import (
-	"sync"
-
-	"github.com/boltdb/bolt"
-	"github.com/hashicorp/raft"
-)
+import "github.com/hashicorp/raft"
 
 type peerStoreWrapper struct {
 	peerStore raft.PeerStore
@@ -26,31 +21,4 @@ func newPeerStore(home string, transport raft.Transport) *peerStoreWrapper {
 	return &peerStoreWrapper{
 		raft.NewJSONPeers(home, transport),
 	}
-}
-
-type peers struct {
-	sync.Mutex
-	peers  map[string]struct{}
-	notify chan string
-	db     *bolt.DB
-}
-
-func (s *peers) Peers() ([]string, error) {
-	s.Lock()
-	var peers []string
-	for p := range s.peers {
-		peers = append(peers, p)
-	}
-	s.Unlock()
-	return peers, nil
-}
-
-func (s *peers) SetPeers(peers []string) error {
-	s.Lock()
-	s.peers = make(map[string]struct{})
-	for _, p := range peers {
-		s.peers[p] = struct{}{}
-	}
-	s.Unlock()
-	return nil
 }
