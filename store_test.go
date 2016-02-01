de031a09f0f2181102b7ab84f3c18eceaf9a7236
Brian Goff <cpuguy83@gmail.com>
2016-01-31 21:20:36 -0500

    more store tests
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- store_test.go
+++ store_test.go
@@ -1,26 +1,25 @@
 package drax
 
 import (
-	"bytes"
-	"fmt"
-	"strings"
 	"testing"
+	"time"
 
-	"github.com/cpuguy83/drax/api"
 	libkvstore "github.com/docker/libkv/store"
+	"github.com/hashicorp/raft"
 )
 
 type testRaft struct {
 	leader bool
 	s      *store
+	index  uint64
 }
 
 func (r *testRaft) IsLeader() bool { return r.leader }
 
 func (r *testRaft) LeaderCh() <-chan interface{} {
-	ch := make(chan interface{})
+	ch := make(chan interface{}, 1)
 	if r.leader {
-		close(ch)
+		ch <- raft.Leader
 	}
 	return ch
 }
@@ -33,40 +32,23 @@ func (r *testRaft) ShutdownCh() <-chan struct{} {
 	return make(chan struct{})
 }
 
-// TODO: decouple raft handling from the store itself
 func (r *testRaft) Apply(b []byte) error {
-	buf := bytes.NewBuffer(b)
-	var req api.Request
-	if err := api.Decode(&req, buf); err != nil {
-		return err
+	l := &raft.Log{
+		Index: r.index,
+		Term:  uint64(0),
+		Type:  raft.LogCommand,
+		Data:  b,
 	}
-
-	switch req.Action {
-	case api.Put:
-		r.s.data.KV[req.Key] = &libkvstore.KVPair{
-			Key:   req.Key,
-			Value: req.Value,
-		}
-	case api.Delete:
-		delete(r.s.data.KV, req.Key)
-	case api.DeleteTree:
-		for k := range r.s.data.KV {
-			if !strings.HasPrefix(k, req.Key) {
-				continue
-			}
-			delete(r.s.data.KV, k)
-			delete(r.s.data.TTLs, k)
-		}
-
-	default:
-		return fmt.Errorf("unknown action")
+	if e := (*storeFSM)(r.s).Apply(l); e != nil {
+		return e.(error)
 	}
+	l.Index++
 	return nil
 }
 
 func TestStoreGet(t *testing.T) {
 	s := newStore()
-	s.r = &testRaft{true, s}
+	s.r = &testRaft{true, s, 0}
 
 	if _, err := s.Get("hello"); err == nil {
 		t.Fatal("expected error getting non-existent key")
@@ -90,7 +72,7 @@ func TestStoreGet(t *testing.T) {
 
 func TestStorePut(t *testing.T) {
 	s := newStore()
-	s.r = &testRaft{true, s}
+	s.r = &testRaft{true, s, 0}
 
 	if err := s.Put("hello", []byte("world"), nil); err != nil {
 		t.Fatal(err)
@@ -107,9 +89,37 @@ func TestStorePut(t *testing.T) {
 	}
 }
 
+func TestStorePutWithTTL(t *testing.T) {
+	s := newStore()
+	s.r = &testRaft{true, s, 0}
+	go s.waitLeader()
+
+	// Currently a 1 second resolution on TTL's
+	if err := s.Put("hello", []byte("world"), &libkvstore.WriteOptions{TTL: 1 * time.Second}); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := s.Get("hello"); err != nil {
+		t.Fatal(err)
+	}
+
+	ticker := time.Tick(50 * time.Millisecond)
+	timeout := time.After(2 * time.Second)
+	for range ticker {
+		select {
+		case <-timeout:
+			t.Fatal("timeout waiting for TTL'd key to be deleted")
+		default:
+		}
+		if _, err := s.Get("hello"); err != nil {
+			break
+		}
+	}
+}
+
 func TestStoreDelete(t *testing.T) {
 	s := newStore()
-	s.r = &testRaft{true, s}
+	s.r = &testRaft{true, s, 0}
 
 	if err := s.Put("hello", []byte("world"), nil); err != nil {
 		t.Fatal(err)
@@ -130,7 +140,7 @@ func TestStoreDelete(t *testing.T) {
 
 func TestStoreExists(t *testing.T) {
 	s := newStore()
-	s.r = &testRaft{true, s}
+	s.r = &testRaft{true, s, 0}
 
 	exists, err := s.Exists("hello")
 	if err != nil {
@@ -157,7 +167,7 @@ func TestStoreExists(t *testing.T) {
 
 func TestStoreList(t *testing.T) {
 	s := newStore()
-	s.r = &testRaft{true, s}
+	s.r = &testRaft{true, s, 0}
 
 	if _, err := s.List("hello"); err == nil {
 		t.Fatal("expected error looking up non-existent key")
@@ -187,7 +197,7 @@ func TestStoreList(t *testing.T) {
 
 func TestStoreDeleteTree(t *testing.T) {
 	s := newStore()
-	s.r = &testRaft{true, s}
+	s.r = &testRaft{true, s, 0}
 
 	if err := s.Put("hello", []byte("world"), nil); err != nil {
 		t.Fatal(err)
